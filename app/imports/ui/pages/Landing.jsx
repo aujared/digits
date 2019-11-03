8bbfde2d25425e09122e60b07443d84d83c56727
Jared Au <aujared@hawaii.edu>
2019-11-02 17:19:55 -1000

    Attempt 2 17 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='digitBG'>
+          <Grid stackable centered columns={3} container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='users' size='huge'> </Icon>
+              <Header as='h1'>Multiple Users</Header>
+              <Header as='h3'>This address book enables any number of
+                users to register and save their business contacts.
+                You can only see the contacts you have created.
+              </Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='file alternate' size='huge'> </Icon>
+              <Header as='h1'>Contact Details</Header>
+              <Header as='h3'>For each contact, you can save their name, address and phone number
+              </Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='calendar check' size='huge'> </Icon>
+              <Header as='h1'>Multiple Users</Header>
+              <Header as='h3'>Each time you make contact with a contact
+                you can wrtie a note that summarized the conversation. This note is saved along with a timestamp of the
+                contact.
+              </Header>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+          </Grid>
+        </div>
     );
   }
 }
